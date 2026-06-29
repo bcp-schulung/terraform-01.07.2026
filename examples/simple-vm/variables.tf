2b9057aae47aa9c73f213a299380e5c9c6992fbd
Ben Cöppicus <ben.coeppicus@it-scholar.com>
2026-06-29 13:16:51 +0200

    Add Terraform configuration for a simple VM setup with networking

---
+++ examples/simple-vm/variables.tf
@@ -0,0 +1,28 @@
+variable "resource_group_name" {
+  description = "The resource group name"
+  type = string
+  default = "rg-tf-lab"
+}
+
+variable "prefix" {
+  description = "Prefix for all resources"
+  type = string
+}
+
+variable "vm_name" {
+  description = "Name of the VM"
+  type = string
+  default = "demo"
+}
+
+variable "vm_size" {
+    description = "The size of the actual VM"
+    type = string
+  default = "Standard_B1s"
+}
+
+variable "admin_username" {
+  description = "Administrator username for the Linux VM. Must not be a reserved Linux username."
+  type        = string
+  default     = "azureuser"
+}
