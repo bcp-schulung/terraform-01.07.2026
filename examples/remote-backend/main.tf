977d9aafc2f3b05469830c0241423d130163ac23
Ben Cöppicus <ben.coeppicus@it-scholar.com>
2026-06-30 09:54:31 +0200

    Add Azure virtual network resource to remote backend example

--- examples/remote-backend/main.tf
+++ examples/remote-backend/main.tf
@@ -1,3 +1,10 @@
 data "azurerm_resource_group" "main" {
   name = var.resource_group_name
 }
+
+resource "azurerm_virtual_network" "main" {
+  name                = "test-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+}
